2678b91436c3b847756d0e7459ed944f77b6223b
abacaxihliffy <jackazeredo23@gmail.com>
2025-09-12 20:35:29 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// troque 'tcl-promocao' se o nome do repo mudar
 export default defineConfig({
   plugins: [react()],
+  base: '/tcl-promocao/',
 })
